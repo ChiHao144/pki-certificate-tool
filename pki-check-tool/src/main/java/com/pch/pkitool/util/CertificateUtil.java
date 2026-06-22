5dc9be86eee4d79a03f3624422a534887eaaaa39
Chi Hao <haopc1404@gmail.com>
2026-06-22 09:28:20 +0700

    add new CA via manual input

--- pki-check-tool/src/main/java/com/pch/pkitool/util/CertificateUtil.java
+++ pki-check-tool/src/main/java/com/pch/pkitool/util/CertificateUtil.java
@@ -14,55 +14,55 @@ import org.bouncycastle.asn1.x500.style.IETFUtils;
 
 public class CertificateUtil {
 
-    private static final Map<String, List<String>> CA_PROVIDERS = Map.ofEntries(
-            Map.entry("VNPT", List.of("VNPT")),
-            Map.entry("Viettel", List.of("VIETTEL")),
-            Map.entry("BKAV", List.of("BKAV")),
-            Map.entry("FPT", List.of("FPT")),
-            Map.entry("CA2", List.of("CA2", "NACENCOMM")),
-            Map.entry("SAFECA", List.of("SAFE")),
-            Map.entry("SmartSign", List.of("SMARTSIGN", "VINA-CA", "VINA")),
-            Map.entry("Newtel", List.of("NEWTEL", "NEW-CA")),
-            Map.entry("EFY", List.of("EFY")),
-            Map.entry("FASTCA", List.of("FASTCA")),
-            Map.entry("MISA-CA", List.of("MISA-CA")),
-            Map.entry("NCCA", List.of("NC-CA")),
-            Map.entry("LCSCA", List.of("LCS-CA")),
-            Map.entry("CMC", List.of("CMC-CA", "CMC")),
-            Map.entry("EASYCA", List.of("EASYCA")),
-            Map.entry("ICA", List.of("I-CA")),
-            Map.entry("LCA", List.of("LA")),
-            Map.entry("HILO", List.of("HILO-CA")),
-            Map.entry("ONECA", List.of("ONE-CA")),
-            Map.entry("WINCA", List.of("WINCA")),
-            Map.entry("VGCA", List.of("Ban Cơ yếu Chính phủ")),
-            Map.entry("MATBAOCA", List.of("MATBAO-CA")),
-            Map.entry("ECA", List.of("E-CA")),
-            Map.entry("MOBIFONE", List.of("MOBIFONE CA")),
-            Map.entry("VNPAY", List.of("VNPAY-CA")),
-            Map.entry("IntrustCA", List.of("INTRUSTCA")),
-            Map.entry("MKCA", List.of("MK CA")),
-            Map.entry("TrustCA", List.of("TRUSTCA")));
+//    private static final Map<String, List<String>> CA_PROVIDERS = Map.ofEntries(
+//            Map.entry("VNPT", List.of("VNPT")),
+//            Map.entry("Viettel", List.of("VIETTEL")),
+//            Map.entry("BKAV", List.of("BKAV")),
+//            Map.entry("FPT", List.of("FPT")),
+//            Map.entry("CA2", List.of("CA2", "NACENCOMM")),
+//            Map.entry("SAFECA", List.of("SAFE")),
+//            Map.entry("SmartSign", List.of("SMARTSIGN", "VINA-CA", "VINA")),
+//            Map.entry("Newtel", List.of("NEWTEL", "NEW-CA")),
+//            Map.entry("EFY", List.of("EFY")),
+//            Map.entry("FASTCA", List.of("FASTCA")),
+//            Map.entry("MISA-CA", List.of("MISA-CA")),
+//            Map.entry("NCCA", List.of("NC-CA")),
+//            Map.entry("LCSCA", List.of("LCS-CA")),
+//            Map.entry("CMC", List.of("CMC-CA", "CMC")),
+//            Map.entry("EASYCA", List.of("EASYCA")),
+//            Map.entry("ICA", List.of("I-CA")),
+//            Map.entry("LCA", List.of("LA")),
+//            Map.entry("HILO", List.of("HILO-CA")),
+//            Map.entry("ONECA", List.of("ONE-CA")),
+//            Map.entry("WINCA", List.of("WINCA")),
+//            Map.entry("VGCA", List.of("Ban Cơ yếu Chính phủ")),
+//            Map.entry("MATBAOCA", List.of("MATBAO-CA")),
+//            Map.entry("ECA", List.of("E-CA")),
+//            Map.entry("MOBIFONE", List.of("MOBIFONE CA")),
+//            Map.entry("VNPAY", List.of("VNPAY-CA")),
+//            Map.entry("IntrustCA", List.of("INTRUSTCA")),
+//            Map.entry("MKCA", List.of("MK CA")),
+//            Map.entry("TrustCA", List.of("TRUSTCA")));
 
     public static String detectCAProvider(String issuerDN) {
         try {
-            String dnUpper = issuerDN.toUpperCase();
-
-            for (var entry : CA_PROVIDERS.entrySet()) {
-                for (String keyword : entry.getValue()) {
-                    if (dnUpper.contains(keyword)) {
-                        return entry.getKey();
-                    }
-                }
-            }
-
-            for (var entry : CA_PROVIDERS.entrySet()) {
-                for (String keyword : entry.getValue()) {
-                    if (dnUpper.contains(keyword.toUpperCase(Locale.ROOT))) {
-                        return entry.getKey();
-                    }
-                }
-            }
+//            String dnUpper = issuerDN.toUpperCase();
+//
+//            for (var entry : CA_PROVIDERS.entrySet()) {
+//                for (String keyword : entry.getValue()) {
+//                    if (dnUpper.contains(keyword)) {
+//                        return entry.getKey();
+//                    }
+//                }
+//            }
+//
+//            for (var entry : CA_PROVIDERS.entrySet()) {
+//                for (String keyword : entry.getValue()) {
+//                    if (dnUpper.contains(keyword.toUpperCase(Locale.ROOT))) {
+//                        return entry.getKey();
+//                    }
+//                }
+//            }
 
             X500Name x500Name = new X500Name(issuerDN);
 
